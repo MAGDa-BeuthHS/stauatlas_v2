9ad86bafccb161c8a49c81281535e047c046588d
Jacoba <jacoba.brandner@web.de>
2017-11-08 16:07:03 +0100

    show the traffic light

--- stauatlas/src/App.js
+++ stauatlas/src/App.js
@@ -4,13 +4,38 @@ import './App.css';
 import MapView from './MapView/MapView.js';
 import SideBar from './SideBar/SideBar.js';
 import Header from './Header/Header.js';
+import { getTrafficInfos } from './trafficService'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebarOpen: false
+      sidebarOpen: false,
+      trafficData: []
     };
+  }
+
+  componentDidMount() {
+    /* 
+    [{
+      averageSpeed: 35,
+      latitude:     51.118823986987536,
+      longitude:    13.76635460086739,
+      relativeSpeed:70,
+      sensor_id:    1178,
+      speed_limit:  50,
+    }]
+    */
+    getTrafficInfos()
+    .then(traffic => {
+      this.setState((prevState) => {
+        if(prevState !== traffic) {
+          return {
+            trafficData: traffic
+          }
+        }
+      })
+    });
   }
 
   handleViewSidebar = () => {
@@ -26,7 +51,10 @@ class App extends Component {
       <div className="stauatlas-app">
         <Header handleViewSidebar={this.handleViewSidebar}/>
         <SideBar isOpen={this.state.sidebarOpen} />
-        <MapView position={[51.050407,13.737262]} zoom={13} />
+        <MapView 
+          position={[51.050407,13.737262]}
+          zoom={13}
+          traffic={this.state.trafficData}/>
       </div>
     );
   }
--- stauatlas/src/MapView/MapView.js
+++ stauatlas/src/MapView/MapView.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Circle } from 'react-leaflet';
 
-
-import { getTrafficInfos } from '../trafficService'
 import './map.css';
 
 const osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>'
@@ -14,28 +12,22 @@ const layer = 'https://api.mapbox.com/styles/v1/mapbox/basic-v9/tiles/256/{z}/{x
 const accessToken = 'pk.eyJ1Ijoic2FraW1hIiwiYSI6ImNqMXo5Z3F2bTAwZnUyeG41N210eWRtbGUifQ.vQjupMfaIwku2OMNsaPTDA'
 const url = layer + '?access_token=' + accessToken;
 
-/* 
-[{
-  averageSpeed: 35,
-  latitude:     51.118823986987536,
-  longitude:    13.76635460086739,
-  relativeSpeed:70,
-  sensor_id:    1178,
-  speed_limit:  50,
-}]
-*/
-const testData = getTrafficInfos()
-  .then(traffic => {
-    console.log('traffic data: ',traffic);
-  });
 
-
-const MapView = ({ position, zoom }) => (
+const MapView = ({ position, zoom, traffic}) => (
   <Map center={position} zoom={zoom} zoomControl={false}>
     <TileLayer
       url={url}
       attribution={attribution}
     />
+
+    {traffic.map(light => (
+      <Circle 
+        className="legend-red"
+        key={light.sensor_id}
+        center={[light.latitude, light.longitude]}
+        radius={11}
+      />
+    ))}
     <div className="box box-legend">
       <h5>Farblegende</h5>
       <ul className="map-legend">
