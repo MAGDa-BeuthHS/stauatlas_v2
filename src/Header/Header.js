26816352786c1c0f0c949a05a1c9a2f85ea9920a
hulkoba <jacoba.brandner@web.de>
2017-12-08 15:13:01 +0100

    style(header): clean up header.css

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,9 +4,9 @@ import './header.css';
 export const Header = ({ isOpen, handleViewSidebar }) => {
   const openClass = isOpen ? 'open' : '';
   return (
-    <div className={`header ${openClass}`}>
+    <div className={`box header ${openClass}`}>
       <a href="#" className="sidebar-toggle" onClick={handleViewSidebar}>
-        <span className="fa fa-angle-double-right" aria-hidden="true" />
+        <span className="fa fa-fw fa-2x fa-angle-double-right" aria-hidden="true" />
       </a>
     </div>
   );
